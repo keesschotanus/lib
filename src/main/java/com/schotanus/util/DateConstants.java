2ab0d2a34edb148b3fec1e23e959e1cda4312813
keesschotanus <kees.schotanus@planet.nl>
2023-07-29 10:30:24 +0200

    Corrected all log statements

---
+++ src/main/java/com/schotanus/util/DateConstants.java
@@ -0,0 +1,301 @@
+/*
+ * Copyright (C) 2010 Kees Schotanus
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+package com.schotanus.util;
+
+/**
+ * A collection of date related constants.<br>
+ * These constants prevent Sonar from complaining about the use of meaningless constants.
+ */
+public final class DateConstants {
+
+    public static final int DAY_1 = 1;
+
+    public static final int DAY_2 = 2;
+
+    public static final int DAY_3 = 3;
+
+    public static final int DAY_4 = 4;
+
+    public static final int DAY_5 = 5;
+
+    public static final int DAY_6 = 6;
+
+    public static final int DAY_7 = 7;
+
+    public static final int DAY_8 = 8;
+
+    public static final int DAY_9 = 9;
+
+    public static final int DAY_10 = 10;
+
+    public static final int DAY_11 = 11;
+
+    public static final int DAY_12 = 12;
+
+    public static final int DAY_13 = 13;
+
+    public static final int DAY_14 = 14;
+
+    public static final int DAY_15 = 15;
+
+    public static final int DAY_16 = 16;
+
+    public static final int DAY_17 = 17;
+
+    public static final int DAY_18 = 18;
+
+    public static final int DAY_19 = 19;
+
+    public static final int DAY_20 = 20;
+
+    public static final int DAY_21 = 21;
+
+    public static final int DAY_22 = 22;
+
+    public static final int DAY_23 = 23;
+
+    public static final int DAY_24 = 24;
+
+    public static final int DAY_25 = 25;
+
+    public static final int DAY_26 = 26;
+
+    public static final int DAY_27 = 27;
+
+    public static final int DAY_28 = 28;
+
+    public static final int DAY_29 = 29;
+
+    public static final int DAY_30 = 30;
+
+    public static final int DAY_31 = 31;
+
+    /** Birth year of Einstein */
+    public static final int YEAR_1879 = 1879;
+    public static final int YEAR_1900 = 1900;
+    public static final int YEAR_1901 = 1901;
+    public static final int YEAR_1902 = 1902;
+    public static final int YEAR_1903 = 1903;
+    public static final int YEAR_1904 = 1904;
+    public static final int YEAR_1905 = 1905;
+    public static final int YEAR_1906 = 1906;
+    public static final int YEAR_1907 = 1907;
+    public static final int YEAR_1908 = 1908;
+    public static final int YEAR_1909 = 1909;
+    public static final int YEAR_1910 = 1910;
+    public static final int YEAR_1911 = 1911;
+    public static final int YEAR_1912 = 1912;
+    public static final int YEAR_1913 = 1913;
+    public static final int YEAR_1914 = 1914;
+    public static final int YEAR_1915 = 1915;
+    public static final int YEAR_1916 = 1916;
+    public static final int YEAR_1917 = 1917;
+    public static final int YEAR_1918 = 1918;
+    public static final int YEAR_1919 = 1919;
+    public static final int YEAR_1920 = 1920;
+    public static final int YEAR_1921 = 1921;
+    public static final int YEAR_1922 = 1922;
+    public static final int YEAR_1923 = 1923;
+    public static final int YEAR_1924 = 1924;
+    public static final int YEAR_1925 = 1925;
+    public static final int YEAR_1926 = 1926;
+    public static final int YEAR_1927 = 1927;
+    public static final int YEAR_1928 = 1928;
+    public static final int YEAR_1929 = 1929;
+    public static final int YEAR_1930 = 1930;
+    public static final int YEAR_1931 = 1931;
+    public static final int YEAR_1932 = 1932;
+    public static final int YEAR_1933 = 1933;
+    public static final int YEAR_1934 = 1934;
+    public static final int YEAR_1935 = 1935;
+    public static final int YEAR_1936 = 1936;
+    public static final int YEAR_1937 = 1937;
+    public static final int YEAR_1938 = 1938;
+    public static final int YEAR_1939 = 1939;
+    public static final int YEAR_1940 = 1940;
+    public static final int YEAR_1941 = 1941;
+    public static final int YEAR_1942 = 1942;
+    public static final int YEAR_1943 = 1943;
+    public static final int YEAR_1944 = 1944;
+    public static final int YEAR_1945 = 1945;
+    public static final int YEAR_1946 = 1946;
+    public static final int YEAR_1947 = 1947;
+    public static final int YEAR_1948 = 1948;
+    public static final int YEAR_1949 = 1949;
+    public static final int YEAR_1950 = 1950;
+    public static final int YEAR_1951 = 1951;
+    public static final int YEAR_1952 = 1952;
+    public static final int YEAR_1953 = 1953;
+    public static final int YEAR_1954 = 1954;
+    public static final int YEAR_1955 = 1955;
+    public static final int YEAR_1956 = 1956;
+    public static final int YEAR_1957 = 1957;
+    public static final int YEAR_1958 = 1958;
+    public static final int YEAR_1959 = 1959;
+    public static final int YEAR_1960 = 1960;
+    public static final int YEAR_1961 = 1961;
+    public static final int YEAR_1962 = 1962;
+    public static final int YEAR_1963 = 1963;
+    public static final int YEAR_1964 = 1964;
+    public static final int YEAR_1965 = 1965;
+    public static final int YEAR_1966 = 1966;
+    public static final int YEAR_1967 = 1967;
+    public static final int YEAR_1968 = 1968;
+    public static final int YEAR_1969 = 1969;
+    /** Year 1970 The Unix Epoch. */
+    public static final int YEAR_1970 = 1970;
+    public static final int YEAR_1971 = 1971;
+    public static final int YEAR_1972 = 1972;
+    public static final int YEAR_1973 = 1973;
+    public static final int YEAR_1974 = 1974;
+    public static final int YEAR_1975 = 1975;
+    public static final int YEAR_1976 = 1976;
+    public static final int YEAR_1977 = 1977;
+    public static final int YEAR_1978 = 1978;
+    public static final int YEAR_1979 = 1979;
+    public static final int YEAR_1980 = 1980;
+    public static final int YEAR_1981 = 1981;
+    public static final int YEAR_1982 = 1982;
+    public static final int YEAR_1983 = 1983;
+    public static final int YEAR_1984 = 1984;
+    public static final int YEAR_1985 = 1985;
+    public static final int YEAR_1986 = 1986;
+    public static final int YEAR_1987 = 1987;
+    public static final int YEAR_1988 = 1988;
+    public static final int YEAR_1989 = 1989;
+    public static final int YEAR_1990 = 1990;
+    public static final int YEAR_1991 = 1991;
+    public static final int YEAR_1992 = 1992;
+    public static final int YEAR_1993 = 1993;
+    public static final int YEAR_1994 = 1994;
+    public static final int YEAR_1995 = 1995;
+    public static final int YEAR_1996 = 1996;
+    public static final int YEAR_1997 = 1997;
+    public static final int YEAR_1998 = 1998;
+    public static final int YEAR_1999 = 1999;
+    public static final int YEAR_2000 = 2000;
+    public static final int YEAR_2001 = 2001;
+    public static final int YEAR_2002 = 2002;
+    public static final int YEAR_2003 = 2003;
+    public static final int YEAR_2004 = 2004;
+    public static final int YEAR_2005 = 2005;
+    public static final int YEAR_2006 = 2006;
+    public static final int YEAR_2007 = 2007;
+    public static final int YEAR_2008 = 2008;
+    public static final int YEAR_2009 = 2009;
+    public static final int YEAR_2010 = 2010;
+    public static final int YEAR_2011 = 2011;
+    public static final int YEAR_2012 = 2012;
+    public static final int YEAR_2013 = 2013;
+    public static final int YEAR_2014 = 2014;
+    public static final int YEAR_2015 = 2015;
+    public static final int YEAR_2016 = 2016;
+    public static final int YEAR_2017 = 2017;
+    public static final int YEAR_2018 = 2018;
+    public static final int YEAR_2019 = 2019;
+    public static final int YEAR_2020 = 2020;
+    public static final int YEAR_2021 = 2021;
+    public static final int YEAR_2022 = 2022;
+    public static final int YEAR_2023 = 2023;
+    public static final int YEAR_2024 = 2024;
+    public static final int YEAR_2025 = 2025;
+    public static final int YEAR_2026 = 2026;
+    public static final int YEAR_2027 = 2027;
+    public static final int YEAR_2028 = 2028;
+    public static final int YEAR_2029 = 2029;
+    public static final int YEAR_2030 = 2030;
+    public static final int YEAR_2031 = 2031;
+    public static final int YEAR_2032 = 2032;
+    public static final int YEAR_2033 = 2033;
+    public static final int YEAR_2034 = 2034;
+    public static final int YEAR_2035 = 2035;
+    public static final int YEAR_2036 = 2036;
+    public static final int YEAR_2037 = 2037;
+    public static final int YEAR_2038 = 2038;
+    public static final int YEAR_2039 = 2039;
+    public static final int YEAR_2040 = 2040;
+    public static final int YEAR_2041 = 2041;
+    public static final int YEAR_2042 = 2042;
+    public static final int YEAR_2043 = 2043;
+    public static final int YEAR_2044 = 2044;
+    public static final int YEAR_2045 = 2045;
+    public static final int YEAR_2046 = 2046;
+    public static final int YEAR_2047 = 2047;
+    public static final int YEAR_2048 = 2048;
+    public static final int YEAR_2049 = 2049;
+    public static final int YEAR_2050 = 2050;
+    public static final int YEAR_2051 = 2051;
+    public static final int YEAR_2052 = 2052;
+    public static final int YEAR_2053 = 2053;
+    public static final int YEAR_2054 = 2054;
+    public static final int YEAR_2055 = 2055;
+    public static final int YEAR_2056 = 2056;
+    public static final int YEAR_2057 = 2057;
+    public static final int YEAR_2058 = 2058;
+    public static final int YEAR_2059 = 2059;
+    public static final int YEAR_2060 = 2060;
+    public static final int YEAR_2061 = 2061;
+    public static final int YEAR_2062 = 2062;
+    public static final int YEAR_2063 = 2063;
+    public static final int YEAR_2064 = 2064;
+    public static final int YEAR_2065 = 2065;
+    public static final int YEAR_2066 = 2066;
+    public static final int YEAR_2067 = 2067;
+    public static final int YEAR_2068 = 2068;
+    public static final int YEAR_2069 = 2069;
+    public static final int YEAR_2070 = 2070;
+    public static final int YEAR_2071 = 2071;
+    public static final int YEAR_2072 = 2072;
+    public static final int YEAR_2073 = 2073;
+    public static final int YEAR_2074 = 2074;
+    public static final int YEAR_2075 = 2075;
+    public static final int YEAR_2076 = 2076;
+    public static final int YEAR_2077 = 2077;
+    public static final int YEAR_2078 = 2078;
+    public static final int YEAR_2079 = 2079;
+    public static final int YEAR_2080 = 2080;
+    public static final int YEAR_2081 = 2081;
+    public static final int YEAR_2082 = 2082;
+    public static final int YEAR_2083 = 2083;
+    public static final int YEAR_2084 = 2084;
+    public static final int YEAR_2085 = 2085;
+    public static final int YEAR_2086 = 2086;
+    public static final int YEAR_2087 = 2087;
+    public static final int YEAR_2088 = 2088;
+    public static final int YEAR_2089 = 2089;
+    public static final int YEAR_2090 = 2090;
+    public static final int YEAR_2091 = 2091;
+    public static final int YEAR_2092 = 2092;
+    public static final int YEAR_2093 = 2093;
+    public static final int YEAR_2094 = 2094;
+    public static final int YEAR_2095 = 2095;
+    public static final int YEAR_2096 = 2096;
+    public static final int YEAR_2097 = 2097;
+    public static final int YEAR_2098 = 2098;
+    public static final int YEAR_2099 = 2099;
+
+    /**
+     * Private constructor prevents creation of an instance outside this class.
+     */
+    private DateConstants() {
+        // Deliberately empty.
+    }
+
+}
---
+++ src/main/java/com/schotanus/util/TimeConstants.java
@@ -0,0 +1,521 @@
+/*
+ * Copyright (C) 2010 Kees Schotanus
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+package com.schotanus.util;
+
+/**
+ * A collection of time related constants.<br>
+ * These constants prevent Sonar from complaining about the use of meaningless constants.
+ */
+public final class TimeConstants {
+
+    /**
+     * Number of milliseconds in a second.
+     */
+    public static final long MILLISECONDS_IN_A_SECOND = 1000;
+
+    /**
+     * Number of seconds in a minute.
+     */
+    public static final int SECONDS_IN_A_MINUTE = 60;
+
+    /**
+     * Number of minutes in an hour.
+     */
+    public static final int MINUTES_IN_AN_HOUR = 60;
+
+    /**
+     * Number of seconds in an hour.
+     */
+    public static final int SECONDS_IN_AN_HOUR = SECONDS_IN_A_MINUTE * MINUTES_IN_AN_HOUR;
+
+    /**
+     * Number of hours in a day.
+     */
+    public static final int HOURS_IN_A_DAY = 24;
+
+    /**
+     * Number of seconds in a day.
+     */
+    public static final int SECONDS_IN_A_DAY = HOURS_IN_A_DAY * SECONDS_IN_AN_HOUR;
+
+    /**
+     * Number of milliseconds in a minute.
+     */
+    public static final long MILLISECONDS_IN_A_MINUTE = MILLISECONDS_IN_A_SECOND * SECONDS_IN_A_MINUTE;
+
+    /**
+     * Number of milliseconds per hour.
+     */
+    public static final long MILLISECONDS_IN_AN_HOUR = MILLISECONDS_IN_A_SECOND * SECONDS_IN_AN_HOUR;
+
+    /**
+     * Number of milliseconds per day.
+     */
+    public static final long MILLISECONDS_IN_A_DAY = MILLISECONDS_IN_AN_HOUR * HOURS_IN_A_DAY;
+
+    /** Hour 0. */
+    public static final int HOUR_0 = 0;
+
+    /** Hour 1. */
+    public static final int HOUR_1 = 1;
+
+    /** Hour 2. */
+    public static final int HOUR_2 = 2;
+
+    /** Hour 3. */
+    public static final int HOUR_3 = 3;
+
+    /** Hour 4. */
+    public static final int HOUR_4 = 4;
+
+    /** Hour 5. */
+    public static final int HOUR_5 = 5;
+
+    /** Hour 6. */
+    public static final int HOUR_6 = 6;
+
+    /** Hour 7. */
+    public static final int HOUR_7 = 7;
+
+    /** Hour 8. */
+    public static final int HOUR_8 = 8;
+
+    /** Hour 9. */
+    public static final int HOUR_9 = 9;
+
+    /** Hour 10. */
+    public static final int HOUR_10 = 10;
+
+    /** Hour 11. */
+    public static final int HOUR_11 = 11;
+
+    /** Hour 12. */
+    public static final int HOUR_12 = 12;
+
+    /** Hour 13. */
+    public static final int HOUR_13 = 13;
+
+    /** Hour 14. */
+    public static final int HOUR_14 = 14;
+
+    /** Hour 15. */
+    public static final int HOUR_15 = 15;
+
+    /** Hour 16. */
+    public static final int HOUR_16 = 16;
+
+    /** Hour 17. */
+    public static final int HOUR_17 = 17;
+
+    /** Hour 18. */
+    public static final int HOUR_18 = 18;
+
+    /** Hour 19. */
+    public static final int HOUR_19 = 19;
+
+    /** Hour 20. */
+    public static final int HOUR_20 = 20;
+
+    /** Hour 21. */
+    public static final int HOUR_21 = 21;
+
+    /** Hour 22. */
+    public static final int HOUR_22 = 22;
+
+    /** Hour 23. */
+    public static final int HOUR_23 = 23;
+
+    /** Hour 24. */
+    public static final int HOUR_24 = 24;
+
+    /** Minute 0. */
+    public static final int MINUTE_0 = 0;
+
+    /** Minute 1. */
+    public static final int MINUTE_1 = 1;
+
+    /** Minute 2. */
+    public static final int MINUTE_2 = 2;
+
+    /** Minute 3. */
+    public static final int MINUTE_3 = 3;
+
+    /** Minute 4. */
+    public static final int MINUTE_4 = 4;
+
+    /** Minute 5. */
+    public static final int MINUTE_5 = 5;
+
+    /** Minute 6. */
+    public static final int MINUTE_6 = 6;
+
+    /** Minute 7. */
+    public static final int MINUTE_7 = 7;
+
+    /** Minute 8. */
+    public static final int MINUTE_8 = 8;
+
+    /** Minute 9. */
+    public static final int MINUTE_9 = 9;
+
+    /** Minute 10. */
+    public static final int MINUTE_10 = 10;
+
+    /** Minute 11. */
+    public static final int MINUTE_11 = 11;
+
+    /** Minute 12. */
+    public static final int MINUTE_12 = 12;
+
+    /** Minute 13. */
+    public static final int MINUTE_13 = 13;
+
+    /** Minute 14. */
+    public static final int MINUTE_14 = 14;
+
+    /** Minute 15. */
+    public static final int MINUTE_15 = 15;
+
+    /** Minute 16. */
+    public static final int MINUTE_16 = 16;
+
+    /** Minute 17. */
+    public static final int MINUTE_17 = 17;
+
+    /** Minute 18. */
+    public static final int MINUTE_18 = 18;
+
+    /** Minute 19. */
+    public static final int MINUTE_19 = 19;
+
+    /** Minute 20. */
+    public static final int MINUTE_20 = 20;
+
+    /** Minute 21. */
+    public static final int MINUTE_21 = 21;
+
+    /** Minute 22. */
+    public static final int MINUTE_22 = 22;
+
+    /** Minute 23. */
+    public static final int MINUTE_23 = 23;
+
+    /** Minute 24. */
+    public static final int MINUTE_24 = 24;
+
+    /** Minute 25. */
+    public static final int MINUTE_25 = 25;
+
+    /** Minute 26. */
+    public static final int MINUTE_26 = 26;
+
+    /** Minute 27. */
+    public static final int MINUTE_27 = 27;
+
+    /** Minute 28. */
+    public static final int MINUTE_28 = 28;
+
+    /** Minute 29. */
+    public static final int MINUTE_29 = 29;
+
+    /** Minute 30. */
+    public static final int MINUTE_30 = 30;
+
+    /** Minute 31. */
+    public static final int MINUTE_31 = 31;
+
+    /** Minute 32. */
+    public static final int MINUTE_32 = 32;
+
+    /** Minute 33. */
+    public static final int MINUTE_33 = 33;
+
+    /** Minute 34. */
+    public static final int MINUTE_34 = 34;
+
+    /** Minute 35. */
+    public static final int MINUTE_35 = 35;
+
+    /** Minute 36. */
+    public static final int MINUTE_36 = 36;
+
+    /** Minute 37. */
+    public static final int MINUTE_37 = 37;
+
+    /** Minute 38. */
+    public static final int MINUTE_38 = 38;
+
+    /** Minute 39. */
+    public static final int MINUTE_39 = 39;
+
+    /** Minute 40. */
+    public static final int MINUTE_40 = 40;
+
+    /** Minute 41. */
+    public static final int MINUTE_41 = 41;
+
+    /** Minute 42. */
+    public static final int MINUTE_42 = 42;
+
+    /** Minute 43. */
+    public static final int MINUTE_43 = 43;
+
+    /** Minute 44. */
+    public static final int MINUTE_44 = 44;
+
+    /** Minute 45. */
+    public static final int MINUTE_45 = 45;
+
+    /** Minute 46. */
+    public static final int MINUTE_46 = 46;
+
+    /** Minute 47. */
+    public static final int MINUTE_47 = 47;
+
+    /** Minute 48. */
+    public static final int MINUTE_48 = 48;
+
+    /** Minute 49. */
+    public static final int MINUTE_49 = 49;
+
+    /** Minute 50. */
+    public static final int MINUTE_50 = 50;
+
+    /** Minute 51. */
+    public static final int MINUTE_51 = 51;
+
+    /** Minute 52. */
+    public static final int MINUTE_52 = 52;
+
+    /** Minute 53. */
+    public static final int MINUTE_53 = 53;
+
+    /** Minute 54. */
+    public static final int MINUTE_54 = 54;
+
+    /** Minute 55. */
+    public static final int MINUTE_55 = 55;
+
+    /** Minute 56. */
+    public static final int MINUTE_56 = 56;
+
+    /** Minute 57. */
+    public static final int MINUTE_57 = 57;
+
+    /** Minute 58. */
+    public static final int MINUTE_58 = 58;
+
+    /** Minute 59. */
+    public static final int MINUTE_59 = 59;
+
+    /** Minute 60. */
+    public static final int MINUTE_60 = 60;
+
+    /** Second 0. */
+    public static final int SECOND_0 = 0;
+
+    /** Second 1. */
+    public static final int SECOND_1 = 1;
+
+    /** Second 2. */
+    public static final int SECOND_2 = 2;
+
+    /** Second 3. */
+    public static final int SECOND_3 = 3;
+
+    /** Second 4. */
+    public static final int SECOND_4 = 4;
+
+    /** Second 5. */
+    public static final int SECOND_5 = 5;
+
+    /** Second 6. */
+    public static final int SECOND_6 = 6;
+
+    /** Second 7. */
+    public static final int SECOND_7 = 7;
+
+    /** Second 8. */
+    public static final int SECOND_8 = 8;
+
+    /** Second 9. */
+    public static final int SECOND_9 = 9;
+
+    /** Second 10. */
+    public static final int SECOND_10 = 10;
+
+    /** Second 11. */
+    public static final int SECOND_11 = 11;
+
+    /** Second 12. */
+    public static final int SECOND_12 = 12;
+
+    /** Second 13. */
+    public static final int SECOND_13 = 13;
+
+    /** Second 14. */
+    public static final int SECOND_14 = 14;
+
+    /** Second 15. */
+    public static final int SECOND_15 = 15;
+
+    /** Second 16. */
+    public static final int SECOND_16 = 16;
+
+    /** Second 17. */
+    public static final int SECOND_17 = 17;
+
+    /** Second 18. */
+    public static final int SECOND_18 = 18;
+
+    /** Second 19. */
+    public static final int SECOND_19 = 19;
+
+    /** Second 20. */
+    public static final int SECOND_20 = 20;
+
+    /** Second 21. */
+    public static final int SECOND_21 = 21;
+
+    /** Second 22. */
+    public static final int SECOND_22 = 22;
+
+    /** Second 23. */
+    public static final int SECOND_23 = 23;
+
+    /** Second 24. */
+    public static final int SECOND_24 = 24;
+
+    /** Second 25. */
+    public static final int SECOND_25 = 25;
+
+    /** Second 26. */
+    public static final int SECOND_26 = 26;
+
+    /** Second 27. */
+    public static final int SECOND_27 = 27;
+
+    /** Second 28. */
+    public static final int SECOND_28 = 28;
+
+    /** Second 29. */
+    public static final int SECOND_29 = 29;
+
+    /** Second 30. */
+    public static final int SECOND_30 = 30;
+
+    /** Second 31. */
+    public static final int SECOND_31 = 31;
+
+    /** Second 32. */
+    public static final int SECOND_32 = 32;
+
+    /** Second 33. */
+    public static final int SECOND_33 = 33;
+
+    /** Second 34. */
+    public static final int SECOND_34 = 34;
+
+    /** Second 35. */
+    public static final int SECOND_35 = 35;
+
+    /** Second 36. */
+    public static final int SECOND_36 = 36;
+
+    /** Second 37. */
+    public static final int SECOND_37 = 37;
+
+    /** Second 38. */
+    public static final int SECOND_38 = 38;
+
+    /** Second 39. */
+    public static final int SECOND_39 = 39;
+
+    /** Second 40. */
+    public static final int SECOND_40 = 40;
+
+    /** Second 41. */
+    public static final int SECOND_41 = 41;
+
+    /** Second 42. */
+    public static final int SECOND_42 = 42;
+
+    /** Second 43. */
+    public static final int SECOND_43 = 43;
+
+    /** Second 44. */
+    public static final int SECOND_44 = 44;
+
+    /** Second 45. */
+    public static final int SECOND_45 = 45;
+
+    /** Second 46. */
+    public static final int SECOND_46 = 46;
+
+    /** Second 47. */
+    public static final int SECOND_47 = 47;
+
+    /** Second 48. */
+    public static final int SECOND_48 = 48;
+
+    /** Second 49. */
+    public static final int SECOND_49 = 49;
+
+    /** Second 50. */
+    public static final int SECOND_50 = 50;
+
+    /** Second 51. */
+    public static final int SECOND_51 = 51;
+
+    /** Second 52. */
+    public static final int SECOND_52 = 52;
+
+    /** Second 53. */
+    public static final int SECOND_53 = 53;
+
+    /** Second 54. */
+    public static final int SECOND_54 = 54;
+
+    /** Second 55. */
+    public static final int SECOND_55 = 55;
+
+    /** Second 56. */
+    public static final int SECOND_56 = 56;
+
+    /** Second 57. */
+    public static final int SECOND_57 = 57;
+
+    /** Second 58. */
+    public static final int SECOND_58 = 58;
+
+    /** Second 59. */
+    public static final int SECOND_59 = 59;
+
+    /** Second 60. */
+    public static final int SECOND_60 = 60;
+
+    /**
+     * Private constructor prevents creation of an instance outside this class.
+     */
+    private TimeConstants() {
+        // Deliberately empty.
+    }
+
+}
